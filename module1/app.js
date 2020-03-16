5163ab5d79c0b1f5cdc2015b40e6e3d130c415a2
andreisvist <svistunov_dr@live.ru>
2020-03-16 18:13:53 +0300

    ready

--- module1/app.js
+++ module1/app.js
@@ -9,27 +9,38 @@
     function LunchCheckController($scope) {
         $scope.menu = "";
         $scope.message = "";
+        $scope.myStyleDefault = {};
+        const redStyle = { "color": "red", "border-color": "red", "padding": "4px", "border-style": "solid" };
+        const greenStyle = { "color": "green", "border-color": "green", "padding": "4px", "border-style": "green"} 
 
         $scope.checkIfTooMuch = function() {
             if ($scope.menu.length) {
                 if ($scope.menu.split(',').length > 3) {
                     let totalItems = [];
                     for (let i = 0; i < $scope.menu.split(',').length; i++) {
-                        if ($scope.menu.split(',')[i].length) {
+                        if ($scope.menu.split(',')[i].trim().length > 0) {
                             totalItems.push($scope.menu.split(',')[i]);
                         }
                     }
                     if (totalItems.length > 3) {
                         $scope.message = "Too much!";
+                        $scope.myStyle = greenStyle;
+                    } else if (totalItems.length === 0) {
+                        $scope.message = "Please enter the data first";
+                        $scope.myStyle = redStyle;
                     } else {
                         $scope.message = "Enjoy!";
+                        $scope.myStyle = greenStyle;
                     }
                 } else {
                     $scope.message = "Enjoy!";
+                    $scope.myStyle = greenStyle;
                 }
             } else {
                 $scope.message = "Please enter the data first";
+                $scope.myStyle = redStyle;
             }
+            $scope.menu = "";
         }
     }
 })();
